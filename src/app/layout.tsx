6d6bbf373615740a93acfd559d9745773bd31306
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-04-28 17:27:37 +0800

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Antonio, League_Spartan } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const antonio = Antonio({ subsets: ["latin"] });
+const spartan = League_Spartan({ subsets: ["latin"] });
+
 
 export const metadata = {
   title: 'Create Next App',
@@ -15,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={spartan.className}>{children}</body>
     </html>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,19 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        "background": "hsl(240, 67%, 8%)",
+        "gray": "hsl(240, 17%, 26%)",
+        "light-gray": "hsl(240, 6%, 54%)",
+        "blue": "hsl(194, 48%, 49%)",
+        "yellow": "hsl(33, 82%, 61%)",
+        "purple": "hsl(263, 67%, 51%)",
+        "dark-red": "hsl(10, 63%, 51%)",
+        "red": "hsl(2, 68%, 53%)",
+        "orange": "hsl(17, 73%, 46%)",
+        "light-green": "hsl(169, 73%, 44%)",
+        "bright-blue": "hsl(222, 87%, 56%)"
+      }
     },
   },
   plugins: [],
