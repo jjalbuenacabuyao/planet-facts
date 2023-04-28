7bbe7c8c608717c6fd54d7e83248b53c856b023b
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-04-28 19:28:33 +0800

    Logo component

---
+++ src/app/components/Logo.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link"
+import { Antonio } from "next/font/google";
+
+const antonio = Antonio({subsets: ["latin"]});
+
+const Logo = () => {
+  return (
+    <Link href={"/"}>
+      <span className={`${antonio.className} uppercase text-white text-xl`}>
+        The Planets
+      </span>
+    </Link>
+  )
+}
+
+export default Logo
