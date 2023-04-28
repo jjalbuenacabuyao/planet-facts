84bab4d4ecdc7bea1b1f97f7ae67171f56724fdd
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-04-28 19:28:21 +0800

    header component

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,13 @@
+import Logo from "./Logo"
+import Navigation from "./Navigation"
+
+const Header = () => {
+  return (
+    <header>
+      <Logo />
+      <Navigation />
+    </header>
+  )
+}
+
+export default Header
