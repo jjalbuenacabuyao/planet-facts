1f659f249abf873584d85c44157027b561bc5ab8
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-04-29 18:38:10 +0800

    MenuButton component

---
+++ src/app/components/MenuButton.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import React, { SetStateAction } from 'react';
+import {AiOutlineMenu, AiOutlineClose} from "react-icons/ai";
+
+interface Props {
+  navOpen: boolean;
+  setNavOpen: React.Dispatch<SetStateAction<boolean>>;
+}
+
+function MenuButton({navOpen, setNavOpen}: Props) {
+  return (
+    <button className='block md:hidden' onClick={() => setNavOpen(!navOpen)}>
+      {navOpen ? <AiOutlineClose size={24} /> : <AiOutlineMenu size={24} />}
+    </button>
+  )
+}
+
+export default MenuButton
