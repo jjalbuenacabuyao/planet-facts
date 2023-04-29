41e541b80d5dddb0846f408111f68c4fac292c23
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-04-29 18:37:01 +0800

    add MenuButton

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,11 +1,18 @@
+"use client";
+
+import { useState } from "react";
 import Logo from "./Logo"
 import Navigation from "./Navigation"
+import MenuButton from "./MenuButton";
 
 const Header = () => {
+  const [navOpen, setNavOpen] = useState(false);
+
   return (
-    <header>
+    <header className="py-4 px-6 relative flex items-center justify-between">
       <Logo />
       <Navigation />
+      <MenuButton navOpen={navOpen} setNavOpen={setNavOpen} />
     </header>
   )
 }
