126a8c3cb65a8898caf3104ba1aefbb95c884abb
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-04-29 19:39:28 +0800

    Grid Component

---
+++ src/app/components/Grid.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import SecondaryNavigation from "./SecondaryNavigation"
+
+const Grid = () => {
+  return (
+    <div>
+      <SecondaryNavigation />
+    </div>
+  )
+}
+
+export default Grid
