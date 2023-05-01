1a247189b6589abff92e578b80212e56da368944
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-05-01 17:35:49 +0800

    move the class to the ul

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -1,26 +1,37 @@
 "use client";
 
-import Link from "next/link"
+import Link from "next/link";
 
 interface Props {
   navOpen: boolean;
 }
 
-const Navigation = ({navOpen} : Props) => {
-  const navlinks = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+const Navigation = ({ navOpen }: Props) => {
+  const navlinks = [
+    "Mercury",
+    "Venus",
+    "Earth",
+    "Mars",
+    "Jupiter",
+    "Saturn",
+    "Uranus",
+    "Neptune",
+  ];
   return (
-    <nav className={`absolute inset-0 translate-x-full transition ${navOpen && "translate-x-0"}`}>
-      <ul>
-        {navlinks.map(link => (
+    <nav>
+      <ul className={`absolute inset-0 transition w-full ${
+        navOpen ? "translate-x-0" : "translate-x-full"
+      }`}>
+        {navlinks.map((link) => (
           <li key={link}>
-            <Link href={`/${link.toLowerCase()}`}>
+            <Link href={`/${link === "Mercury" ? "/" : link.toLowerCase()}`}>
               {link}
             </Link>
           </li>
         ))}
       </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Navigation
+export default Navigation;
