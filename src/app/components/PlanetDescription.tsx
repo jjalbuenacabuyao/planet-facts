37cadfe5da354cbb1e0c2f6c5a187e248769b95e
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-05-04 20:47:04 +0800

    larger font-size

--- src/app/components/PlanetDescription.tsx
+++ src/app/components/PlanetDescription.tsx
@@ -14,8 +14,8 @@ const PlanetDescription = () => {
   const { name, overview, structure, geology } = planet;
 
   return (
-    <div className="text-center px-6 flex flex-col gap-4 md:text-left md:col-start-1 md:row-start-2 md:pl-10">
-      <h1 className={`${antonio.className} text-4xl`}>{name}</h1>
+    <div className="text-center px-6 flex flex-col gap-4 md:text-left md:col-start-1 md:row-start-2 md:pl-10 md:gap-6">
+      <h1 className={`${antonio.className} text-4xl md:text-5xl`}>{name}</h1>
       <p className="opacity-70 font-light">
         {activeTab === "Overview"
           ? overview.content
