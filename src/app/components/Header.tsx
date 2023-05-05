06b19407c10046cfc08a9a95fa90d24a08ebf2e8
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-05-05 10:53:52 +0800

    remove padding-bottom on tablet

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
   const [navOpen, setNavOpen] = useState(false);
 
   return (
-    <header className="py-4 px-6 relative flex items-center justify-between border-b-[1px] border-b-gray md:justify-center md:flex-col md:gap-10 md:pt-8">
+    <header className="py-4 px-6 relative flex items-center justify-between border-b-[1px] border-b-gray md:justify-center md:flex-col md:gap-10 md:pt-8 md:pb-0">
       <Logo />
       <Navigation navOpen={navOpen} setNavOpen={setNavOpen} />
       <MenuButton navOpen={navOpen} setNavOpen={setNavOpen} />
