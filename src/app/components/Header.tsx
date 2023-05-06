9193055267adbdb77021d9907d3683d6aaefe596
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-05-06 11:03:39 +0800

    adjust padding-x

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
   const [navOpen, setNavOpen] = useState(false);
 
   return (
-    <header className="relative flex items-center justify-between border-b-[1px] border-b-gray px-6 py-4 md:flex-col md:justify-center md:gap-10 md:pb-0 md:pt-8 lg:flex-row lg:justify-between lg:py-0">
+    <header className="relative mx-auto flex max-w-screen-2xl items-center justify-between border-b-[1px] border-b-gray px-6 py-4 md:flex-col md:justify-center md:gap-10 md:pb-0 md:pt-8 lg:flex-row lg:justify-between lg:px-14 lg:py-0">
       <Logo />
       <Navigation navOpen={navOpen} setNavOpen={setNavOpen} />
       <MenuButton navOpen={navOpen} setNavOpen={setNavOpen} />
